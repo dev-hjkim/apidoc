176000aa9544cd85a692ccf68b41468e0aeea391
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-12-12 20:57:35 +0900

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "celebrity",
+  "version": "1.0.1",
+  "description": "celebrity apiDoc basic example",
+  "title": "celebrity apiDoc",
+  "url": "http://localhost:8000",
+  "sampleUrl": "http://localhost:8000",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-12-10T19:52:53.860Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require('cors');
+var fs = require('fs');
+
+
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/locales', express.static(__dirname + '/locales'));
+app.use('/utils', express.static(__dirname + '/utils'));
+app.use('/', express.static(__dirname + '/'));
+
+
+app.get('/', (req, res) => {
+    console.log("접속되었습니다!!!!!!!!!");
+    fs.readFile('index.html', function (error, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+    //res.sendFile('index.html', { root: __dirname });
+});
+
+
+//4000번 port에 서버
+app.listen(4000, "localhost", () => {
+    console.log('Express server');
+});
